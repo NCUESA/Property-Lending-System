607b96b77a1dc191e689e80de372e9744e28961c
小弘 <tgh120893@gmail.com>
2025-02-23 02:32:52 +0800

    Update AppServiceProvider.php
    修改IP偵測方式

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,21 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         View::composer('*', function ($view) {
-            $clientIp = request()->ip();
+          $clientIp= '127.0.0.1';
+          if (isset($_SERVER['HTTP_CF_CONNECTING_IPV6'])) {
+            $clientIp = $_SERVER['HTTP_CF_CONNECTING_IPV6'];
+          } elseif (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+            $clientIp = $_SERVER['HTTP_CF_CONNECTING_IP'];
+          } elseif (isset($_SERVER["HTTP_X_REAL_IP"])) {
+            $clientIp = $_SERVER["HTTP_X_REAL_IP"];
+          } elseif (isset($_SERVER['HTTP_X_FORWARED_FOR'])) {
+            $clientIp = $_SERVER['HTTP_X_FORWARED_FOR'];
+          } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            $clientIp = $_SERVER['HTTP_CLIENT_IP'];
+          } else {
+            if (isset($_SERVER['REMOTE_ADDR']))
+              $clientIp = $_SERVER['REMOTE_ADDR'];
+          }
             $isAllowed = AuthIP::select('auth_level')
                 ->where('ip', $clientIp)
                 ->first();
