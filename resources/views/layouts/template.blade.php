7963328fbbc6e7fd4b8930e24947f3f884b042be
小弘 <tgh120893@gmail.com>
2025-02-23 23:55:55 +0800

    Update template.blade.php

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -9,8 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="{{ asset('css/index.css') }}" rel="stylesheet">
-    <link rel="shortcut icon" href="{{ asset('/favicon.png') }}">
-    <link rel="bookmark" href="{{ asset('/favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
+    <link rel="bookmark" href="{{ asset('/favicon.ico') }}">
     <title>彰師學生會器材借用網站</title>
 </head>
 
