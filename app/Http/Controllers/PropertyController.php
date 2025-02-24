8e5ff0f74e87db8e7f2780df394081ae024a5d6e
小弘 <tgh120893@gmail.com>
2025-02-24 22:50:15 +0800

    Update PropertyController.php

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -192,7 +192,8 @@ class PropertyController extends Controller
                 // 檢查是否為 HEIC
                 if ($extension === 'heic' || $extension === 'heif') {
                     // 讀取 HEIC 並轉換為 JPG
-                    $img = Image::make($file->getRealPath())->encode('jpg', 90);
+                    $manager = new ImageManager();
+                    $img = $manager->make($file)->encode('jpg', 90);
                     
                     // 儲存轉換後的圖片
                     Storage::put("public/propertyImgs/{$filename}", $img);
@@ -222,7 +223,8 @@ class PropertyController extends Controller
                 // 檢查是否為 HEIC
                 if ($extension === 'heic' || $extension === 'heif') {
                     // 讀取 HEIC 並轉換為 JPG
-                    $img = Image::make($file->getRealPath())->encode('jpg', 90);
+                    $manager = new ImageManager();
+                    $img = $manager->make($file)->encode('jpg', 90);
             
                     // 儲存轉換後的圖片
                     Storage::put("public/propertyImgs/{$filename}", $img);
